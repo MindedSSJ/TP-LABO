d51cdfa00eca73957d8edcf168bedce2c0beaebc
MindedSSJ <cesarvillarroel2122000@gmail.com>
2025-05-12 08:57:31 -0300

    9am 12/05
    Funcion nueva en clase donante

--- clases/donante.py
+++ clases/donante.py
@@ -2,13 +2,17 @@ from paciente import paciente
 
 class donante(paciente):
     
-    def __init__(self, fecha_muerte, hora_muerte, fecha_ablacion, hora_ablacion, lista_organos):
-
+    def __init__(self, nombre, dni, nacimiento, sexo, telefono, tipo_sangre, centro_salud, fecha_muerte, hora_muerte, fecha_ablacion,
+                  hora_ablacion, lista_organos):
+        super().__init__(nombre, dni, nacimiento, sexo, telefono, tipo_sangre, centro_salud)
+        
         self.fecha_muerte = fecha_muerte
         self.hora_muerte = hora_muerte
         self.fecha_ablacion = fecha_ablacion
         self.hora_ablacion = hora_ablacion
-        self.lista_organos = []
+        self.lista_organos = lista_organos
+
+        super().__init__(nombre, dni, nacimiento, sexo, telefono, tipo_sangre, centro_salud)
         
         def tiene_organos(self):
 
@@ -17,4 +21,26 @@ class donante(paciente):
             else:
                 print("{self.lista_organos} son los organos viables para donar del paciente")
                 
+        
+        def organo_donado(self, tipo_organo):
+          """
+            Marca un órgano como donado y lo remueve de la lista de órganos.
+            Si el órgano no está disponible, devuelve un mensaje de error.
+          """
+          for organo in self.lista_organos:
+              if organo.tipo == tipo_organo:
+                  self.lista_organos.remove(organo)
+                  print("Organo '{tipo_organo}'")
+                  return organo
+              else:
+                  print("no se encontro el organo '{tipo_organo}' en la lista del donante")
+                  return None
+                  
+              
+
+
+
+
+
+
                 
