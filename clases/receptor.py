961d304548b28870494cbe9abbc37c3c9f6a4cb4
MindedSSJ <cesarvillarroel2122000@gmail.com>
2025-05-12 09:32:38 -0300

    9:30 12/05
    Cambios en clase Receptor: Nuevas funciones e inicios de otra

--- clases/receptor.py
+++ clases/receptor.py
@@ -2,7 +2,10 @@ from paciente import paciente
 
 class receptor(paciente):
     
-    def __init__(self, organo_necesitado, fecha_listado, prioridad, patologia, estado):
+    def __init__(self, nombre, dni, tipo_sangre, organo_necesitado, nacimiento, sexo, telefono, centro_salud, fecha_listado,
+                  prioridad, patologia, estado):
+        
+        super().__init__(nombre, dni, nacimiento, sexo, telefono, tipo_sangre, centro_salud)
 
         self.organo_necesitado = organo_necesitado
         self.fecha_listado = fecha_listado
@@ -10,3 +13,18 @@ class receptor(paciente):
         self.patologia = patologia
         self.estado = estado
         
+
+        def match(self, donante):
+            #preguntar a agus si no deberiamos hacer tambien que el tipo de sangre coincida para hacer el match
+            
+            if self.organo_necesitado in donante.lista_organo and self.tipo_sangre == donante.tipo_sangre:
+                print("Match encontrado para el paciente {receptor.nombre}, organo otorgano por {donante.nombre}")
+                return True
+            else:
+                print("no hay match para el receptor {receptor.nombre}")
+                return False
+            
+        def cambio_prioridad(self):
+            #preguntar agus jeje
+            return
+            
